5a8ed6e40671a61e3cca2c0a5442debf3a7ed933
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-05-15 20:33:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+/*
+This is part of my front-end development practise. I'm practising since 204 days and counting :) my goal is to reach 300 days of continuous practise.
+
+The faces by the amazing @JustinMezzell -> http://drbl.in/maCB
+
+My website:
+www.ishadeed.com
+
+Follow Me on twitter: @shadeed9
+
+*/
